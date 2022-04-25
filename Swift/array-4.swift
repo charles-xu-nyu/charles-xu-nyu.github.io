350df0bc21e08c6c032af49864712c0fed7c83ea
Charles Xu <73838999+charles-xu-nyu@users.noreply.github.com>
2022-04-25 14:27:03 -0400

    Delete array-4.swift

--- Swift/array-4.swift
+++
@@ -1,9 +0,0 @@
-var someStrs = [String]()
-
-someStrs.append("Apple")
-someStrs.append("Amazon")
-someStrs += ["Google"]
-
-for (index, item) in someStrs.enumerated() {
-   print("Value at index = \(index) is \(item)")
-}
