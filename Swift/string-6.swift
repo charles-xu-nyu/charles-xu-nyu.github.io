85c9de99aad2a797c2086faa73879fb71011734b
Charles Xu <73838999+charles-xu-nyu@users.noreply.github.com>
2022-04-25 14:35:35 -0400

    Delete string-6.swift

--- Swift/string-6.swift
+++
@@ -1,8 +0,0 @@
-var varA = "Hello, Swift!"
-var varB = "Hello, World!"
-
-if varA == varB {
-   print( "\(varA) and \(varB) are equal" )
-} else {
-   print( "\(varA) and \(varB) are not equal" )
-}
