3ac653cd5b9c9d3a79e772738ed63cdba6c8da0b
Charles Xu <73838999+charles-xu-nyu@users.noreply.github.com>
2022-04-25 14:32:55 -0400

    Add files via upload

---
+++ Swift/optional/optional-3.swift
@@ -0,0 +1,9 @@
+var myString:String?
+
+myString = "Hello, Swift!"
+
+if myString != nil {
+   print( myString! )
+} else {
+   print("myString has nil value")
+}
---
+++ Swift/optional/optional-4.swift
@@ -0,0 +1,8 @@
+var myString:String?
+myString = "Hello, Swift!"
+
+if let yourString = myString {
+   print("Your string has - \(yourString)")
+} else {
+   print("Your string does not have a value")
+}
